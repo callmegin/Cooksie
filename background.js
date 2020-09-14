ae4de80b8b5eb7f23678d3fc93c0e3371bb84621
Gin <gintautas.anduzis@gmail.com>
2020-09-14 15:34:17 +0300

    Possible core events

--- background.js
+++ background.js
@@ -1,33 +1,61 @@
 const getUrl = (link) => link.split("/")[2];
 
-chrome.browserAction.onClicked.addListener((tab) => {
-  // No tabs or host permissions needed!
-  console.log("clicked on a tab");
-});
+//Below doesn't work
+//
+// chrome.browserAction.onClicked.addListener((tab) => {
+//   // No tabs or host permissions needed!
+//   console.log("clicked on a tab");
+// });
 
-chrome.browserAction.setBadgeBackgroundColor({ color: "red" });
+chrome.browserAction.setBadgeBackgroundColor({ color: "green" });
+chrome.browserAction.setBadgeText({ text: "..." });
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
-  chrome.tabs.get(activeInfo.tabId, (tab) => {
-    //get current url
-    //     let link = tab.url;
-    let url = new URL(tab.url);
-    let domain = url.hostname;
-    console.log(domain);
-    chrome.cookies.getAll({ path: "www.15min.lt" }, (cookies) => {
-      console.log(cookies);
+const getCookies = () => {
+  chrome.tabs.executeScript(
+    {
+      code: 'performance.getEntriesByType("resource").map(el => el.name)',
+    },
+    (data) => {
+      console.log(data[0].length);
+      data[0].map((urls) => {
+        // console.log(urls);
+      });
+    }
+  );
+};
+
+const getDomain = (info) => {
+  return new Promise((resolve) => {
+    chrome.tabs.get(info.tabId, (tab) => {
+      //get current url
+      //     let link = tab.url;
+      let url = new URL(tab.url);
+      let domain = url.hostname;
+      resolve(domain);
     });
-
-    // for (let i in cookieStores) {
-    //   console.log(i);
-    //   console.log(cookieStores[i]);
-    // }
   });
-  //     chrome.cookies.getAll({ storeId: link }, (cookies) => {
-  //       console.log(cookies);
-  //     });
-  //   });
+};
+// getDomain(activeInfo).then((result) => console.log("activeInfo " + result));
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  // console.clear();
+  console.log("onActivated");
 });
-//Add numbers next to icon
+chrome.tabs.onCreated.addListener((tab) => {
+  // console.clear();
+  console.log("onCreated");
+});
 
-chrome.browserAction.setBadgeText({ text: "10" });
+const waitForPageLoad = () => {
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    console.log("onUpdated");
+    if (
+      changeInfo.url !== "chrome://newtab/" &&
+      typeof changeInfo.url !== "undefined"
+    ) {
+      console.log("PAGE LOAD DETECTED " + tab.url);
+    }
+    if (changeInfo.status === "complete") {
+      console.log("LOADED");
+    }
+  });
+};
